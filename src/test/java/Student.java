0a58b56b9afa43e6abec9480e4bf38465f5cdb8f
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-03-25 14:15:25 -0500

    constructors and methods, student class

---
+++ src/test/java/Student.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Student {
+
+    private String name;
+    private long id;
+    private ArrayList<Integer> grades;
+
+
+
+
+    public Student(String name, long id, ArrayList<Integer> grades) {
+        this.name = name;
+        this.id = id;
+        this.grades = new ArrayList<>();
+    }
+
+    // returns the student's id
+    public long getId() {...}
+
+
+    // returns the student's name
+    public String getName() {...}
+
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade){...);
+    }
+
+    // returns the list of grades
+    public ArrayList<Integer> getGrades() {...};
+
+
+    // returns the average of the students grades
+    public double getGradeAverage(){...};
+}
